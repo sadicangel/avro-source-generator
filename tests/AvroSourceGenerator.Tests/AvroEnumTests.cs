10b6066b8a5ec68591c51fbac8bc00e6226da354
sadicangel <sadicangel@hotmail.com>
2024-12-09 14:46:11 +0000

    Enhance AvroEnumTests with new test cases and updates
    The `AvroEnumTests` class has been updated to include new test cases and functionality. The namespace declaration now includes `System.Text.Json`. The `Generates_Correct_Enum` method has been refined to remove invalid test cases. New test methods have been added to verify the correct generation of namespaces, summaries, and aliases. Invalid schema definitions and error messages have been removed from the `Generates_Correct_Enum_symbols` test cases.
    
    New verified test output files have been added, and existing ones updated, to ensure the generated code adheres to specified configurations without diagnostics errors. These changes provide a more comprehensive test suite for the `AvroSourceGenerator`, ensuring robust and correct code generation.
    
    Additionally, updates to the generated code files for the `TestEnum` and `Wrapper` classes include new and updated XML documentation comments, reflecting the correct generation of enums and records with specified documentation comments.

--- tests/AvroSourceGenerator.Tests/AvroEnumTests.cs
+++ tests/AvroSourceGenerator.Tests/AvroEnumTests.cs
@@ -1,10 +1,10 @@
-﻿namespace AvroSourceGenerator.Tests;
+﻿using System.Text.Json;
+
+namespace AvroSourceGenerator.Tests;
 
 public class AvroEnumTests
 {
     [Theory]
-    [InlineData("null")]
-    [InlineData("\"not-an-array\"")]
     [InlineData("[]")]
     [InlineData("[\"A\"]")]
     [InlineData("[\"A\", \"B\"]")]
@@ -65,4 +65,93 @@ public class AvroEnumTests
         }
         """")
         .UseParameters(accessModifier);
+
+
+    [Theory]
+    [InlineData("false")]
+    [InlineData("true")]
+    public Task Generates_Correct_Namespace(string useCSharpNamespace) => TestHelper.Verify($$""""
+        using System;
+        using AvroSourceGenerator;
+        
+        namespace CSharpNamespace;
+        
+        [Avro(UseCSharpNamespace = {{useCSharpNamespace}})]
+        public partial class Record
+        {
+            public const string AvroSchema = """
+            {
+                "type": "record",
+                "namespace": "SchemaNamespace",
+                "name": "Wrapper",
+                "fields": [
+                    { "name": "EnumField", "type": {
+                        "type": "enum", "name": "TestEnum", "symbols": []
+                    } }
+                ]
+            }
+            """;
+        }
+        """")
+        .UseParameters(useCSharpNamespace);
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData("Single line comment")]
+    [InlineData("Multi\nline\ncomment")]
+    public Task Generates_Correct_Summary(string? doc) => TestHelper.Verify($$""""
+        using System;
+        using AvroSourceGenerator;
+        
+        namespace CSharpNamespace;
+        
+        [Avro]
+        public partial class Record
+        {        
+            public const string AvroSchema = """
+            {
+                "type": "record",
+                "namespace": "SchemaNamespace",
+                "name": "Wrapper",
+                "fields": [
+                    { "name": "EnumField", "type": {
+                        "type": "enum", "name": "TestEnum", "doc": {{(doc is null ? "null" : $"\"{JsonEncodedText.Encode(doc)}\"")}}, "symbols": []
+                    } }
+                ]
+            }
+            """;
+        }
+        """")
+        .UseParameters(doc);
+
+    [Theory]
+    [InlineData("null")]
+    [InlineData("[]")]
+    [InlineData("[\"Alias1\"]")]
+    [InlineData("[\"Alias1\", \"Alias2\"]")]
+    public Task Generates_Correct_Aliases(string aliases) => TestHelper.Verify($$""""
+        using System;
+        using AvroSourceGenerator;
+        
+        namespace CSharpNamespace;
+        
+        [Avro]
+        public partial class Record
+        {
+            public const string AvroSchema = """
+            {
+                "type": "record",
+                "namespace": "SchemaNamespace",
+                "name": "Wrapper",
+                "fields": [
+                    { "name": "EnumField", "type": {
+                        "type": "enum", "name": "TestEnum", "aliases": {{aliases}}, "symbols": []
+                    } }
+                ]
+            }
+            """;
+        }
+        """")
+        .UseParameters(aliases);
 }

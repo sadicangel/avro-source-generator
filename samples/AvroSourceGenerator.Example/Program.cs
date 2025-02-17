b2ad13ef15edcd5f4ad8261563d682ca76683ad9
sadicangel <sadicangel@hotmail.com>
2025-02-17 16:03:40 +0000

    Turned integration tests project into sample project. (#18)

---
+++ samples/AvroSourceGenerator.Example/Program.cs
@@ -0,0 +1,23 @@
+﻿using Avro;
+using com.example.finance;
+
+var transaction = new Transaction
+{
+    id = Guid.NewGuid(),
+    amount = new AvroDecimal(123.45m),
+    currency = "USD",
+    timestamp = DateTime.UtcNow,
+    status = TransactionStatus.COMPLETED,
+    recipientId = "123456",
+    metadata = new Dictionary<string, string>
+    {
+        { "key1", "value1" },
+        { "key2", "value2" },
+    },
+    signature = new Signature(),
+    legacyId = "abc123",
+};
+
+Random.Shared.NextBytes(transaction.signature.Value);
+
+Console.WriteLine(transaction);

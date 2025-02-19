eee3ca2ccdd681131bbcad9fda622e6dd1549bae
sadicangel <sadicangel@hotmail.com>
2025-02-19 22:30:55 +0000

    Adds tests for default values.

--- tests/AvroSourceGenerator.Tests/FieldTests.cs
+++ tests/AvroSourceGenerator.Tests/FieldTests.cs
@@ -2,7 +2,7 @@
 
 public class FieldTests
 {
-    public static MatrixTheoryData<string, string> Primitives = new(
+    public static MatrixTheoryData<string, string> Primitives => new(
         ["record", "error"],
         ["null", "boolean", "int", "long", "float", "double", "bytes", "string"]);
 
@@ -37,6 +37,37 @@ public class FieldTests
                 {
                     "type": ["null", "{{field}}"],
                     "name": "Field"
+                }
+            ]
+        }
+        """;
+        return TestHelper.VerifySourceCode(schema);
+    }
+
+    public static TheoryData<string, string> Defaults => new([
+        ("null", "null"),
+        ("boolean", "true"),
+        ("int", "42"),
+        ("long", "42"),
+        ("float", "42.0"),
+        ("double", "42.0"),
+        ("bytes", @"""\u0034\u0032"""),
+        ("string", @"""FortyTwo"""),
+        ]);
+
+    [Theory]
+    [MemberData(nameof(Defaults))]
+    public Task Verify_Default(string type, string value)
+    {
+        var schema = $$"""
+        {
+            "type": "record",
+            "name": "Record",
+            "fields": [
+                {
+                    "type": "{{type}}",
+                    "name": "Field",
+                    "default": {{value}}
                 }
             ]
         }

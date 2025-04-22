fe5c1fd61ac2a29ccdcd404b64bcf227441c2e86
sadicangel <sadicangel@hotmail.com>
2025-04-22 22:46:20 +0100

    Uncomment tests for unnamed base schemas.

--- tests/AvroSourceGenerator.Tests/UnnamedBaseSchemaTests.cs
+++ tests/AvroSourceGenerator.Tests/UnnamedBaseSchemaTests.cs
@@ -1,69 +1,69 @@
-﻿//using AvroSourceGenerator.Tests.Helpers;
+﻿using AvroSourceGenerator.Tests.Helpers;
 
-//namespace AvroSourceGenerator.Tests;
+namespace AvroSourceGenerator.Tests;
 
-//public sealed class UnnamedBaseSchemaTests
-//{
-//    [Fact]
-//    public Task Verify_Union() => TestHelper.VerifySourceCode("""
-//    [
-//        {
-//            "type": "record",
-//            "name": "Record1",
-//            "namespace": "Namespace1",
-//            "fields": []
-//        },
-//        {
-//            "type": "record",
-//            "name": "Record2",
-//            "namespace": "Namespace2",
-//            "fields": []
-//        }
-//    ]
-//    """);
+public sealed class UnnamedBaseSchemaTests
+{
+    [Fact]
+    public Task Verify_Union() => TestHelper.VerifySourceCode("""
+    [
+        {
+            "type": "record",
+            "name": "Record1",
+            "namespace": "Namespace1",
+            "fields": []
+        },
+        {
+            "type": "record",
+            "name": "Record2",
+            "namespace": "Namespace2",
+            "fields": []
+        }
+    ]
+    """);
 
-//    [Fact]
-//    public Task Verify_Union_No_Named_Schemas_Diagnostic() => TestHelper.VerifyDiagnostic("[]");
+    [Fact]
+    public Task Verify_Union_No_Named_Schemas_Diagnostic() => TestHelper.VerifyDiagnostic("[]");
 
-//    [Fact]
-//    public Task Verify_Array() => TestHelper.VerifySourceCode("""
-//    {
-//        "type": "array",
-//        "items": {
-//            "type": "record",
-//            "name": "Record1",
-//            "namespace": "Namespace1",
-//            "fields": []
-//        }
-//    }
-//    """);
+    [Fact]
+    public Task Verify_Array() => TestHelper.VerifySourceCode("""
+    {
+        "type": "array",
+        "items": {
+            "type": "record",
+            "name": "Record1",
+            "namespace": "Namespace1",
+            "fields": []
+        }
+    }
+    """);
 
-//    [Fact]
-//    public Task Verify_Array_No_Named_Schemas_Diagnostic() => TestHelper.VerifyDiagnostic("""
-//    {
-//        "type": "array",
-//        "items": "string"
-//    }
-//    """);
+    [Fact]
+    public Task Verify_Array_No_Named_Schemas_Diagnostic() => TestHelper.VerifyDiagnostic("""
+    {
+        "type": "array",
+        "items": "string"
+    }
+    """);
 
-//    [Fact]
-//    public Task Verify_Map() => TestHelper.VerifySourceCode("""
-//    {
-//        "type": "map",
-//        "values": {
-//            "type": "record",
-//            "name": "Record1",
-//            "namespace": "Namespace1",
-//            "fields": []
-//        }
-//    }
-//    """);
+    [Fact]
+    public Task Verify_Map() => TestHelper.VerifySourceCode("""
+    {
+        "type": "map",
+        "values": {
+            "type": "record",
+            "name": "Record1",
+            "namespace": "Namespace1",
+            "fields": []
+        }
+    }
+    """);
 
-//    [Fact]
-//    public Task Verify_Map_No_Named_Schemas_Diagnostic() => TestHelper.VerifyDiagnostic("""
-//    {
-//        "type": "array",
-//        "values": "string"
-//    }
-//    """);
-//}
+    [Fact]
+    public Task Verify_Map_No_Named_Schemas_Diagnostic() => TestHelper.VerifyDiagnostic("""
+    {
+        "type": "array",
+        "values": "string"
+    }
+    """);
+}

41396040287574c0d62fa7209bb6c9ae33434838
sadicangel <sadicangel@hotmail.com>
2025-05-02 01:20:36 +0100

    Move local record declaration tests to AttributeRecordDeclarationTests

--- tests/AvroSourceGenerator.Tests/RecordTests.cs
+++ tests/AvroSourceGenerator.Tests/RecordTests.cs
@@ -17,32 +17,6 @@ public sealed class RecordTests
 
     [Theory]
     [InlineData("record"), InlineData("class")]
-    public Task Verify_RecordDeclaration_Local(string recordDeclaration)
-    {
-        var schema = """
-        {
-            "type": "record",
-            "namespace": "SchemaNamespace",
-            "name": "Record",
-            "fields": []
-        }
-        """;
-
-        var source = $$""""
-        using System;
-        using AvroSourceGenerator;
-        
-        namespace SchemaNamespace;
-        
-        [Avro]
-        public partial {{recordDeclaration}} Record;
-        """";
-
-        return TestHelper.VerifySourceCode(schema, source);
-    }
-
-    [Theory]
-    [InlineData("record"), InlineData("class")]
     public Task Verify_RecordDeclaration_Global(string recordDeclaration)
     {
         var schema = """

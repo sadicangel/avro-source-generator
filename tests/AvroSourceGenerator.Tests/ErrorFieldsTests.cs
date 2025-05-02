c85c2b6f7f8866578edb83811c7792b95b036300
sadicangel <sadicangel@hotmail.com>
2025-05-02 01:11:23 +0100

    Rename ErrorTests to ErrorFieldsTests

---
+++ tests/AvroSourceGenerator.Tests/ErrorFieldsTests.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Nodes;
+using AvroSourceGenerator.Tests.Helpers;
+
+namespace AvroSourceGenerator.Tests;
+
+public sealed class ErrorFieldsTests
+{
+    [Theory]
+    [MemberData(nameof(InvalidFields))]
+    public Task Diagnostic(string json)
+    {
+        var schema = TestSchemas.Get("error").With("fields", JsonNode.Parse(json)!).ToString();
+
+        return TestHelper.VerifyDiagnostic(schema);
+    }
+
+    public static TheoryData<string> InvalidFields() => new(
+        ["null", "{}"]);
+}

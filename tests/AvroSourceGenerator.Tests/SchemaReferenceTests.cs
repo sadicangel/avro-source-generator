5f7b4d84beb495a9e6d857146a61829b871a74ba
sadicangel <sadicangel@hotmail.com>
2025-05-05 00:18:45 +0100

    Improves schema reference tests.
    Deletes schema output tests as is does make sense (?) and its alreay covered by unnamed base schema tests.

--- tests/AvroSourceGenerator.Tests/SchemaReferenceTests.cs
+++ tests/AvroSourceGenerator.Tests/SchemaReferenceTests.cs
@@ -21,7 +21,38 @@ public sealed class SchemaReferenceTests
             },
             {
                 "name": "Field2",
-                "type": "This.Is.A.Full.Name"
+                "type": "This.Is.A.Full.Name",
+                "doc": "This is a reference to the record 'Name' by its full name 'This.Is.A.Full.Name'."
+            },
+            {
+                "name": "Field3",
+                "type": {
+                    "type": "record",
+                    "name": "OtherName",
+                    "namespace": "This.Is.A.Full",
+                    "fields": [
+                        {
+                            "name": "Field4",
+                            "type": "Name",
+                            "doc": "This is a reference to the record 'Name' defined in the same namespace as the enclosing type 'OtherName'."
+                        }
+                    ]
+                }
+            }
+        ]
+    }
+    """);
+
+    [Fact]
+    public Task Diagnostic() => TestHelper.VerifyDiagnostic($$"""
+    {
+        "type": "record",
+        "name": "MissingReference",
+        "doc": "This record contains a field that references a type that is not defined in the schema.",
+        "fields": [
+            {
+                "name": "MissingField",
+                "type": "This.Type.Was.Not.Defined.In.The.Schema"
             }
         ]
     }

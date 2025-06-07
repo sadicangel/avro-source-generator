91bd002b2430e7891632b3c192157d8f73d22f0c
sadicangel <sadicangel@hotmail.com>
2025-06-07 11:24:46 +0100

    Add 2 tests to verify how a root union looks like

---
+++ tests/AvroSourceGenerator.Tests/UnionRootAbstractTests.cs
@@ -0,0 +1,44 @@
+﻿using AvroSourceGenerator.Tests.Helpers;
+
+namespace AvroSourceGenerator.Tests;
+
+public sealed class UnionRootAbstractTests
+{
+    [Fact]
+    public Task Verify()
+    {
+        var schema = """
+        [
+            "null",
+            {
+                "type": "record",
+                "name": "EmailContent",
+                "fields": [
+                    { "name": "subject", "type": "string" },
+                    { "name": "body", "type": "string" },
+                    { "name": "recipientEmail", "type": "string" }
+                ]
+            },
+            {
+                "type": "record",
+                "name": "SmsContent",
+                "fields": [
+                    { "name": "message", "type": "string" },
+                    { "name": "phoneNumber", "type": "string" }
+                ]
+            },
+            {
+                "type": "record",
+                "name": "PushContent",
+                "fields": [
+                    { "name": "title", "type": "string" },
+                    { "name": "message", "type": "string" },
+                    { "name": "deviceToken", "type": "string" }
+                ]
+            }
+        ]
+        """;
+
+        return TestHelper.VerifySourceCode(schema);
+    }
+}

7f8737b09d30fec00c926cb09723b144238f7d9a
Paulo Henriques <paulosrhenriques@gmail.com>
2025-07-13 23:23:28 +0200

    Add documentation to Variant classes on which types can be used (#52)
    Co-authored-by: paulo <paulo@DESKTOP-PAULO>

--- src/AvroSourceGenerator/Schemas/AbstractRecordSchema.cs
+++ src/AvroSourceGenerator/Schemas/AbstractRecordSchema.cs
@@ -6,7 +6,20 @@ namespace AvroSourceGenerator.Schemas;
 internal sealed record class AbstractRecordSchema(
     SchemaName SchemaName,
     ImmutableArray<AvroSchema> DerivedSchemas)
-    : TopLevelSchema(SchemaType.Abstract, default, SchemaName, null, ImmutableSortedDictionary<string, JsonElement>.Empty)
+    : TopLevelSchema(
+        SchemaType.Abstract,
+        default,
+        SchemaName,
+        GetDefaultDocumentation(DerivedSchemas),
+        ImmutableSortedDictionary<string, JsonElement>.Empty)
 {
     public override void WriteTo(Utf8JsonWriter writer, HashSet<SchemaName> writtenSchemas, string? containingNamespace) { }
+
+    private static string GetDefaultDocumentation(ImmutableArray<AvroSchema> derivedSchemas)
+    {
+        var docStart = "Represents a union schema that can be one of the following types: \n<list type=\"bullet\">";
+
+        var items = derivedSchemas.Select(schema => $"\n<item><see cref=\"{schema.CSharpName.FullName}\"/></item>\n");
+        return docStart + string.Join(string.Empty, items) + "</list>";
+    }
 }

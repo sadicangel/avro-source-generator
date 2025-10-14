738ba4d8612b0c98a3a39bd3b3b3f5606d79d0ef
sadicangel <sadicangel@hotmail.com>
2025-10-14 13:06:13 +0100

    Remove support for `alias` metadata (`AvroAliasesAttribute`). (#63)
    It's still parsed and validated but we no longer emit the custom attribute

--- src/AvroSourceGenerator.Attributes/AvroAliasesAttribute.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace AvroSourceGenerator;
-
-[AttributeUsage(AttributeTargets.Class | AttributeTargets.Enum | AttributeTargets.Property)]
-public sealed class AvroAliasesAttribute(params string[] aliases) : Attribute
-{
-    public string[] Aliases { get; } = aliases;
-}

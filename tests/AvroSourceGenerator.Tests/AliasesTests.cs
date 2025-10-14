b42278985952d67316cf9a06e77c22e7b171bf4f
sadicangel <sadicangel@hotmail.com>
2025-10-14 13:01:21 +0100

    Remove support for `alias` metadata (`AvroAliasesAttribute`).
    It's still parsed and validated but we no longer emit the custom attribute

--- tests/AvroSourceGenerator.Tests/AliasesTests.cs
+++ tests/AvroSourceGenerator.Tests/AliasesTests.cs
@@ -6,15 +6,6 @@ namespace AvroSourceGenerator.Tests;
 public sealed class AliasesTests
 {
     [Theory]
-    [MemberData(nameof(ValidAliasesSchemaPairs))]
-    public Task Verify(string[] aliases, string schemaType)
-    {
-        var schema = TestSchemas.Get(schemaType).With("aliases", aliases).ToString();
-
-        return TestHelper.VerifySourceCode(schema);
-    }
-
-    [Theory]
     [MemberData(nameof(InvalidAliasesSchemaPairs))]
     public Task Diagnostic(string json, string schemaType)
     {

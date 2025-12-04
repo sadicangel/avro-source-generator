f0700c0b6ca5cd3c52c57357ac0367697d477033
sadicangel <sadicangel@hotmail.com>
2025-12-04 11:39:32 +0000

    Add verify tests for array and map schemas (#70)
    Enhanced test coverage for AvroSourceGenerator by adding new test cases for `record` and `error` schema types with required and nullable `array<string>` and `map<string>` fields.

---
+++ tests/AvroSourceGenerator.Tests/FieldCollectionTests.cs
@@ -0,0 +1,30 @@
+﻿namespace AvroSourceGenerator.Tests;
+
+public class FieldCollectionTests
+{
+    public static MatrixTheoryData<string, string> Primitives => new(["record", "error"], ["array<string>", "map<string>"]);
+
+    [Theory]
+    [MemberData(nameof(Primitives))]
+    public Task Verify(string @class, string field)
+    {
+        var schema = $$"""
+            {
+                "type": "{{@class}}",
+                "name": "Class",
+                "namespace": "SchemaNamespace",
+                "fields": [
+                    {
+                        "type": {{TestSchemas.Get(field)}},
+                        "name": "Field"
+                    },
+                    {
+                        "type": ["null", {{TestSchemas.Get(field)}}],
+                        "name": "NullableField"
+                    }
+                ]
+            }
+            """;
+        return VerifySourceCode(schema);
+    }
+}

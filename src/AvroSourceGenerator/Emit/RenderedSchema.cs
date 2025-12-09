6cea8c1b8248f27e02fffe21ac4dedb5c6871182
sadicangel <sadicangel@hotmail.com>
2025-12-09 00:16:14 +0000

    Add render step
    This step caches the rendering of each file. The emitter will now only output source files.

---
+++ src/AvroSourceGenerator/Emit/RenderedSchema.cs
@@ -0,0 +1,3 @@
+﻿namespace AvroSourceGenerator.Emit;
+
+internal readonly record struct RenderedSchema(string HintName, string SourceText);
---
+++ src/AvroSourceGenerator/Emit/Renderer.cs
@@ -0,0 +1,48 @@
+﻿using System.Text.Json;
+using AvroSourceGenerator.Configuration;
+using AvroSourceGenerator.Diagnostics;
+using AvroSourceGenerator.Parsing;
+using AvroSourceGenerator.Registry;
+using AvroSourceGenerator.Schemas;
+
+namespace AvroSourceGenerator.Emit;
+
+internal static class Renderer
+{
+    public static RenderResult Render((AvroFile avroFile, RenderSettings renderSettings) source, CancellationToken cancellationToken)
+    {
+        var (avroFile, settings) = source;
+
+        var diagnostics = avroFile.Diagnostics.AddRange(settings.Diagnostics);
+
+        if (!avroFile.IsValid || !settings.IsValid)
+        {
+            return new RenderResult([], diagnostics);
+        }
+
+        try
+        {
+            var schemaRegistry = SchemaRegistry.Register(
+                schema: avroFile.Json,
+                avroLibrary: settings.AvroLibrary,
+                languageVersion: settings.LanguageVersion,
+                useNullableReferenceTypes: settings.LanguageFeatures.HasFlag(LanguageFeatures.NullableReferenceTypes));
+
+            // We should get no render errors, so we don't have to handle anything else.
+            var schemas = AvroTemplate.Render(schemaRegistry, settings);
+
+            return new RenderResult(schemas, diagnostics);
+        }
+        catch (JsonException ex)
+        {
+            diagnostics = diagnostics.Add(InvalidJsonDiagnostic.Create(LocationInfo.FromException(avroFile.Path, avroFile.Text, ex), ex.Message));
+        }
+        catch (InvalidSchemaException ex)
+        {
+            // TODO: We can probably get a better location for the error.
+            diagnostics = diagnostics.Add(InvalidSchemaDiagnostic.Create(LocationInfo.FromSourceFile(avroFile.Path, avroFile.Text), ex.Message));
+        }
+
+        return new RenderResult([], diagnostics);
+    }
+}

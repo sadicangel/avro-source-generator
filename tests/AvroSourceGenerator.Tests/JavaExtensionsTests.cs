da3f1a804ebce4f1f8e68c2f00adcea361faf5b5
sadicangel <sadicangel@hotmail.com>
2026-01-14 14:19:03 +0000

    Add tests for Avro Java-specific extensions
    Test handling for Avro schemas with Java extensions (`avro.java.string`, `java-class`, `java-key-class`).

---
+++ tests/AvroSourceGenerator.Tests/JavaExtensionsTests.cs
@@ -0,0 +1,57 @@
+﻿namespace AvroSourceGenerator.Tests;
+
+public sealed class JavaExtensionsTests
+{
+    [Fact]
+    public Task Verify() => VerifySourceCode(
+        """
+        {
+          "name": "StringBehaviorTest",
+          "namespace": "org.apache.parquet.avro",
+          "type": "record",
+          "fields": [
+            {
+              "name": "default_class",
+              "type": "string"
+            },
+            {
+              "name": "string_class",
+              "type": {
+                "type": "string",
+                "avro.java.string": "String"
+              }
+            },
+            {
+              "name": "stringable_class",
+              "type": {
+                "type": "string",
+                "java-class": "java.math.BigDecimal"
+              }
+            },
+            {
+              "name": "default_map",
+              "type": {
+                "type": "map",
+                "values": "int"
+              }
+            },
+            {
+              "name": "string_map",
+              "type": {
+                "type": "map",
+                "values": "int",
+                "avro.java.string": "String"
+              }
+            },
+            {
+              "name": "stringable_map",
+              "type": {
+                "type": "map",
+                "values": "int",
+                "java-key-class": "java.math.BigDecimal"
+              }
+            }
+          ]
+        }
+        """);
+}

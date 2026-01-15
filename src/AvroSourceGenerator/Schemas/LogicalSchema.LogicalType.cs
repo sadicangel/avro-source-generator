4710c247f6300b811fa6c607e0e252f942238c98
sadicangel <sadicangel@hotmail.com>
2026-01-15 17:48:35 +0000

    Refactorings for logical types (#83)

---
+++ src/AvroSourceGenerator/Schemas/LogicalSchema.LogicalType.cs
@@ -0,0 +1,15 @@
+﻿namespace AvroSourceGenerator.Schemas;
+
+internal static class LogicalType
+{
+    public const string Date = "date";
+    public const string Decimal = "decimal";
+    public const string Duration = "duration";
+    public const string TimeMicros = "time-micros";
+    public const string TimeMillis = "time-millis";
+    public const string TimestampMicros = "timestamp-micros";
+    public const string TimestampMillis = "timestamp-millis";
+    public const string LocalTimestampMicros = "local-timestamp-micros";
+    public const string LocalTimestampMillis = "local-timestamp-millis";
+    public const string Uuid = "uuid";
+}

019fb207519cf414b2cd26b395935c5a65e20ef9
sadicangel <sadicangel@hotmail.com>
2026-01-19 22:32:38 +0000

    Rename `AbstractRecord` to `Variant`
    The generate type is no longer an abstract class but an interface.

--- src/AvroSourceGenerator/Schemas/UnionSchema.cs
+++ src/AvroSourceGenerator/Schemas/UnionSchema.cs
@@ -17,4 +17,35 @@ internal sealed record class UnionSchema(
             schema.WriteTo(writer, writtenSchemas, containingNamespace);
         writer.WriteEndArray();
     }
+
+    // TODO: Can we extend this to Fixed and Error types in the future?
+    public bool SupportsVariant()
+    {
+        if (Schemas is [] or [_] or [{ Type: SchemaType.Null }, _] or [_, { Type: SchemaType.Null }])
+        {
+            // Empty union, single type union, or union with nulls only are not eligible for generating abstract base records.
+            return false;
+        }
+
+        // Check if all schemas are either Record or Null, and at least one is not Null.
+        return Schemas.All(x => x.Type is SchemaType.Record or SchemaType.Null)
+            && Schemas.Any(x => x.Type is not SchemaType.Null);
+    }
+
+    public UnionSchema WithVariant(VariantSchema variant)
+    {
+        // TODO: Can we extend this to Fixed and Error types in the future?
+        foreach (var record in Schemas.OfType<RecordSchema>())
+        {
+            record.InheritsFrom = variant;
+        }
+
+        return this with
+        {
+            CSharpName = new CSharpName(
+                variant.CSharpName.Name + (IsNullable ? "?" : ""),
+                variant.CSharpName.Namespace),
+            UnderlyingSchema = variant
+        };
+    }
 }

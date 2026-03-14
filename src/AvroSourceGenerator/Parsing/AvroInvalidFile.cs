417f0660efc9a546cd5dfcb41ba27d7eeb0cb039
sadicangel <sadicangel@hotmail.com>
2026-03-14 00:40:16 +0000

    Refactor Avro file handling with IAvroFile interface
    These allows us to support different avro file types.

---
+++ src/AvroSourceGenerator/Parsing/AvroInvalidFile.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Immutable;
+using System.Text.Json;
+using AvroSourceGenerator.Diagnostics;
+using AvroSourceGenerator.Exceptions;
+
+namespace AvroSourceGenerator.Parsing;
+
+internal sealed record class AvroInvalidFile(string Path, string? Text, ImmutableArray<DiagnosticInfo> Diagnostics) : IAvroFile
+{
+    public bool Equals(AvroInvalidFile? other) => other is not null && Path == other.Path;
+
+    public override int GetHashCode() => Path?.GetHashCode() ?? 0;
+
+    public static IAvroFile Empty(string path) =>
+        new AvroInvalidFile(path, null, [InvalidJsonDiagnostic.Create(LocationInfo.FromSourceFile(path, null), "The file is empty.")]);
+
+    public static IAvroFile Invalid(string path, string? text, JsonException exception) =>
+        new AvroInvalidFile(path, text, [InvalidJsonDiagnostic.Create(LocationInfo.FromException(path, text, exception), exception.Message)]);
+
+    public static IAvroFile Invalid(string path, string? text, InvalidSchemaException exception) =>
+        new AvroInvalidFile(path, text, [InvalidJsonDiagnostic.Create(LocationInfo.FromSourceFile(path, text), exception.Message)]);
+}
---
+++ src/AvroSourceGenerator/Parsing/AvroSchemaFile.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Immutable;
+using System.Text.Json;
+using AvroSourceGenerator.Diagnostics;
+
+namespace AvroSourceGenerator.Parsing;
+
+internal sealed record class AvroSchemaFile(string Path, string Text) : IAvroFile
+{
+    public JsonElement Json { get; init; } = ParseJson(Text);
+
+    public ImmutableArray<DiagnosticInfo> Diagnostics => [];
+
+    private static JsonElement ParseJson(string text)
+    {
+        using var jsonDocument = JsonDocument.Parse(text!);
+        return jsonDocument.RootElement.Clone();
+    }
+
+    public bool Equals(AvroSchemaFile? other) => other is not null && Path == other.Path && Text == other.Text;
+
+    public override int GetHashCode() => HashCode.Combine(Path, Text);
+}
